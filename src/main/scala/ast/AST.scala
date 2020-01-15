eba555e90c004365c2e0841f04af469c690418d7
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-01-15 15:39:29 +0900

    First commit

---
+++ src/main/scala/ast/AST.scala
@@ -0,0 +1,12 @@
+package ast
+
+trait AST
+trait Component
+trait Definition
+trait HasType {
+  //TODO: tpe field and related methods will be implemented
+}
+
+case class ModuleDef(name: String, ) extends AST
+
+
