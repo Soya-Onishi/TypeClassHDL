bf8f12e351cef017c1ecf0f2eb0b93e0d5fe0e7b
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-01-19 22:02:42 +0900

    Replace case class field type, List[T], into Array[T]
    List[T] is heavier class than Array ones

--- src/main/scala/tchdl/ast/AST.scala
+++ src/main/scala/tchdl/ast/AST.scala
@@ -11,47 +11,63 @@ trait BlockElem
 trait Expression extends AST with BlockElem with HasType
 trait SupportParamElem extends AST
 
+trait WorkingAST extends AST
+
 trait HasType {
   //TODO: tpe field and related methods will be implemented
 }
 
 case class CompilationUnit(filename: Option[String], topDefs: Array[AST with Definition]) extends AST
 
-case class ClassDef(name: String, supportParams: List[SupportParamElem], methods: List[MethodDef]) extends Definition
-case class InterfaceDef(name: String, supportParams: List[SupportParamElem], components: List[Component]) extends Definition
-case class Implement(className: TypeTree, structName: TypeTree, supportParams: List[SupportParamElem], methods: List[MethodDef]) extends Definition
+case class ClassDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], methods: Array[MethodDef]) extends Definition
+case class InterfaceDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], components: Array[Component]) extends Definition
+case class Implement(className: TypeTree, target: TypeTree, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], methods: Array[Component]) extends Definition
 
-case class ModuleDef(name: String, components: List[AST with Component]) extends Definition
-case class StructDef(name: String, supportParams: List[SupportParamElem], fields: List[FieldDef]) extends Definition
+case class ModuleDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], passedModules: Array[FieldDef], components: Array[Component]) extends Definition
+case class StructDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[FieldDef], fields: Array[FieldDef]) extends Definition
 case class FieldDef(flag: Modifier, name: String, tpeTree: TypeTree) extends Definition with SupportParamElem with HasType
-case class EnumDef(name: String, supportParams: List[SupportParamElem], fields: List[EnumFieldDef]) extends Definition
-case class EnumFieldDef(name: String, tpes: List[TypeTree]) extends Definition
+case class EnumDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], fields: Array[EnumFieldDef]) extends Definition
+case class EnumFieldDef(name: String, tpes: Array[TypeTree]) extends Definition
 
 case class AlwaysDef(name: String, blk: Block) extends Definition with Component
-case class MethodDef(name: String, supportParams: List[SupportParamElem], params: List[FieldDef], retTpe: TypeTree, blk: Option[Block]) extends Definition with Component with HasType
+case class MethodDef(name: String, hp: Array[FieldDef], tp: Array[TypeDef], bounds: Array[Bound], params: Array[FieldDef], retTpe: TypeTree, blk: Option[Block]) extends Definition with Component with HasType
 case class ValDef(flag: Modifier, name: String, tpeTree: Option[TypeTree], expr: Option[Expression]) extends Definition with Component with BlockElem
-case class StageDef(name: String, params: List[FieldDef], retTpe: TypeTree, states: List[StateDef], blk: Block) extends Definition with Component with HasType
+case class StageDef(name: String, params: Array[FieldDef], retTpe: TypeTree, states: Array[StateDef], blk: Array[BlockElem]) extends Definition with Component with HasType
 case class StateDef(name: String, blk: Block) extends Definition
 
+case class TypeDef(name: String) extends Definition
+
+case class Bound(target: String, constraints: Array[TypeTree]) extends AST
+
 case class Ident(name: String) extends Expression
-case class Apply(expr: Expression, supportArgs: List[Expression ], args: List[Expression]) extends Expression
+case class Apply(name: Expression, hp: Array[Expression], tp: Array[TypeTree], args: Array[Expression]) extends Expression
 case class Select(expr: Expression, name: String) extends Expression
-case class Block(elems: List[BlockElem], last: Expression) extends Expression
-case class Construct(name: TypeTree, pairs: List[ConstructPair]) extends Expression
+case class Block(elems: Array[BlockElem], last: Expression) extends Expression
+case class Construct(name: TypeTree, pairs: Array[ConstructPair]) extends Expression
 case class ConstructPair(name: String, expr: Expression) extends AST
 case class Self() extends Expression
 case class IfExpr(cond: Expression, conseq: Expression, alt: Option[Expression]) extends Expression
-case class Match(expr: Expression, cases: List[Case]) extends Expression
+case class Match(expr: Expression, cases: Array[Case]) extends Expression
 case class Case(cond: AST, conseq: Block) extends AST
 case class BitLiteral(value: BigInt, length: Int) extends Expression
 case class IntLiteral(value: Int) extends Expression
 case class UnitLiteral() extends Expression
 case class StringLiteral(str: String) extends Expression
 
-case class Finish() extends AST with Expression
-case class Goto(target: String) extends AST with Expression
-case class Generate(target: String, params: List[Expression]) extends Expression
-case class Relay(target: String, params: List[Expression]) extends Expression
+case class Finish() extends Expression
+case class Goto(target: String) extends Expression
+case class Generate(target: String, params: Array[Expression]) extends Expression
+case class Relay(target: String, params: Array[Expression]) extends Expression
 
-case class TypeTree(name: String, supportParam: List[SupportParamElem]) extends AST with SupportParamElem with HasType
+case class TypeTree(name: String, hp: Array[Expression], tp: Array[TypeTree]) extends AST with SupportParamElem with HasType
 
+object WorkingAST {
+  case class HardwareParam(hp: Array[FieldDef]) extends WorkingAST
+  case class TypeParam(tp: Array[TypeDef]) extends WorkingAST
+  case class Bounds(bounds: Array[Bound]) extends WorkingAST
+  case class Inner(inner: Component) extends WorkingAST
+  case class FieldDefs(fields: Array[FieldDef]) extends WorkingAST
+  case class StageBody(state: Array[StateDef], block: Array[BlockElem]) extends WorkingAST
+  case class Modifiers(modifier: Modifier) extends WorkingAST
+  case class ComponentBody(name: String, tpe: Option[TypeTree], expr: Option[Expression]) extends WorkingAST
+}
