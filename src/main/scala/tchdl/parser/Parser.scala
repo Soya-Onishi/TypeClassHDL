8665c7106e3476c7a47b07c01d08d4d8d99db1b4
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-01-19 22:03:37 +0900

    Add visitor pattern rules for parser

---
+++ src/main/scala/tchdl/parser/Parser.scala
@@ -0,0 +1,20 @@
+package tchdl.parser
+
+import tchdl.ast._
+import org.antlr.v4.runtime._
+import org.antlr.v4.runtime.tree._
+import tchdl.antlr._
+import java.io.FileInputStream
+
+object Parser {
+  def parse(filename: String) = {
+    val input = CharStreams.fromFileName(filename)
+    val lexer= new TchdlLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new TchdlParser(tokens)
+    val tree = parser.compilation_unit()
+
+    val cu = parser.compilation_unit()
+
+  }
+}
