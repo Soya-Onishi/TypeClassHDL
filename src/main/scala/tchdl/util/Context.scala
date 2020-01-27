92b33566f4e47bb034f9c892008f4f6e5d814dae
SoyaOhnishi <soya.ohnishi@gmail.com>
2020-01-27 18:12:53 +0900

    Add context to summarize declares

---
+++ src/main/scala/tchdl/util/Context.scala
@@ -0,0 +1,31 @@
+package tchdl.util
+
+import scala.collection.mutable
+
+case class Context(
+  parent: Option[Context],
+  namespace: Vector[String],
+) {
+  val declares = mutable.Map[String, Seq[Symbol]]()
+
+  def enter(name: String, symbol: Symbol): Unit = declares.get(name) match {
+    case None => declares(name) = Seq(symbol)
+    case Some(symbols) => declares(name) = symbols :+ symbol
+  }
+
+  def lookup(name: String): LookupResult = declares.get(name) match {
+    case Some(seq) => LookupResult.LookupSuccess(seq)
+    case None => parent match {
+      case Some(parent) => parent.lookup(name)
+      case None => LookupResult.LookupFailure(name)
+    }
+  }
+}
+
+trait LookupResult
+object LookupResult {
+  case class LookupSuccess(symbols: Seq[Symbol]) extends LookupResult
+  case class LookupFailure(name: String) extends LookupResult
+}
+
+
