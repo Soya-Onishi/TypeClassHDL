ece0bce78597c646b3de9a2c4013f3faaf37fc7b
SoyaOhnishi <soya.ohnishi@gmail.com>
2020-01-28 10:41:35 +0900

    Fix Modifier construction process

--- src/main/scala/tchdl/util/Modifier.scala
+++ src/main/scala/tchdl/util/Modifier.scala
@@ -1,10 +1,10 @@
 package tchdl.util
 
-trait Modifier {
-  val value: BigInt
+class Modifier(_value: Option[BigInt] = None) {
+  val value: BigInt = _value.getOrElse(BigInt(1) << Modifier.count)
 
-  def |(that: Modifier): Modifier = new Modifier { val value: BigInt = this.value | that.value }
-  def &(that: Modifier): Modifier = new Modifier { val value: BigInt = this.value & that.value }
+  def |(that: Modifier): Modifier = new Modifier(Some(this.value | that.value))
+  def &(that: Modifier): Modifier = new Modifier(Some(this.value & that.value))
   def hasFlag(flag: Modifier): Boolean = (this.value & flag.value) > 0
   def hasNoFlag(flag: Modifier): Boolean = !this.hasFlag(flag)
 }
@@ -26,16 +26,21 @@ object Modifier {
       case (acc, name) => acc | apply(name)
     }
 
+  private var _count = 1
+  def count: Int = {
+    val c = _count
+    _count = _count + 1
+    c
+  }
 
-  private val seed = BigInt(1)
-
-  case object NoModifier extends Modifier { val value = 0x0 }
-  case object Input extends Modifier      { val value = seed << 0 }
-  case object Internal extends Modifier   { val value = seed << 1 }
-  case object Output extends Modifier     { val value = seed << 2 }
-  case object Register extends Modifier   { val value = seed << 3 }
-  case object Public extends Modifier     { val value = seed << 4 }
-  case object Module extends Modifier     { val value = seed << 5 }
-  case object Sibling extends Modifier    { val value = seed << 6 }
-  case object Parent extends Modifier     { val value = seed << 7 }
+  case object NoModifier extends Modifier(Some(0))
+  case object Input extends Modifier
+  case object Internal extends Modifier
+  case object Output extends Modifier
+  case object Register extends Modifier
+  case object Public extends Modifier
+  case object Module extends Modifier
+  case object Sibling extends Modifier
+  case object Parent extends Modifier
+  case object Param extends Modifier
 }
