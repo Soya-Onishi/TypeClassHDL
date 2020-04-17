f7d5a350bf98e2b7165d926cfef20f911e401eeb
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-04-17 17:41:40 +0900

    Fix some points in Symbol

--- src/main/scala/tchdl/util/Symbol.scala
+++ src/main/scala/tchdl/util/Symbol.scala
@@ -1,28 +1,27 @@
 package tchdl.util
 
-import tchdl.ast.Definition
+import tchdl.ast.Expression
 
-abstract class Symbol(protected var owner: Option[Symbol], __tpe: Type) {
+abstract class Symbol(ctx: Context, __tpe: Type) {
   val name: String
-  val namespace: Vector[String]
-
-  def getOwner: Option[Symbol] = owner
-  def setOnwer(owner: Symbol): Unit = this.owner = Some(owner)
+  val namespace: NameSpace = ctx.namespace
+  val fullPath: NameSpace = namespace.append(name)
 
   private var isAlreadyReferenced = false
   private var _tpe: Type = __tpe
   def setTpe(tpe: Type): Unit = _tpe = tpe
-  def tpe: Either[Error, Type] = {
+  def tpe: Type = {
+    val isReferenced = isAlreadyReferenced
+    isAlreadyReferenced = true
+
     _tpe match {
-      case _: Type.TypeGenerator if isAlreadyReferenced =>
+      case _: Type.TypeGenerator if isReferenced =>
         ???
       case gen: Type.TypeGenerator =>
-        isAlreadyReferenced = true
         val tpe = gen.generate()
         _tpe = tpe
-        Right(tpe)
-      case tpe =>
-        Right(tpe)
+        tpe
+      case tpe => tpe
     }
   }
 
@@ -31,38 +30,49 @@ abstract class Symbol(protected var owner: Option[Symbol], __tpe: Type) {
   def addFlag(flag: Modifier): this.type = { _flag |= flag; this }
   def hasFlag(flag: Modifier): Boolean = { _flag.hasFlag(flag) }
   def flag: Modifier = _flag
+
+  override def equals(obj: Any): Boolean = symEq(obj)
+  protected def symEq(obj: Any): Boolean
+
+  def asTypeSymbol: Symbol.TypeSymbol = this.asInstanceOf[Symbol.TypeSymbol]
+  def asTermSymbol: Symbol.TermSymbol = this.asInstanceOf[Symbol.TermSymbol]
 }
 
 object Symbol {
-  class TypeSymbol(val name: String, val namespace: Vector[String], owner: Option[Symbol], tpe: Type) extends Symbol(owner, tpe)
+  class TypeSymbol(val name: String, tpe: Type, ctx: Context) extends Symbol(ctx, tpe) {
+    override def symEq(obj: Any): Boolean = obj match {
+      case obj: TypeSymbol => this.fullPath == obj.fullPath
+      case _ => false
+    }
+
+    def makeRefType(tp: Vector[Expression]): Either[Error, Type.RefType] = tpe match {
+      case tpe: Type.DeclaredType =>
+        def lengthCheck: Either[Error, Unit] = {
+          val paramLength = tpe.hardwareParam.length + tpe.typeParam.length
+          if(paramLength != tp.length)
+
+        }
+
+
+
+    }
+  }
+
   object TypeSymbol {
-    def apply(name: String, namespace: Vector[String], tpe: Type, owner: Option[Symbol] = None): TypeSymbol =
-      new TypeSymbol(name, namespace, owner, tpe)
+    def apply(name: String, tpe: Type, ctx: Context): TypeSymbol =
+      new TypeSymbol(name, tpe, ctx)
   }
 
-  /*
-  class ClassSymbol(name: String, namespace: Vector[String], owner: Option[Symbol], tpe: Type) extends TypeSymbol(name, namespace, owner, tpe)
-  object ClassSymbol {
-    def apply(name: String, namespace: Vector[String], tpe: Type, owner: Option[Symbol] = None): ClassSymbol =
-      new ClassSymbol(name, namespace, owner, tpe)
+  class TermSymbol(val name: String, tpe: Type, ctx: Context) extends Symbol(ctx, tpe) {
+    override def symEq(obj: Any): Boolean = obj match {
+      case obj: TermSymbol => this.fullPath == obj.fullPath
+      case _ => false
+    }
   }
 
-  class StructSymbol(name: String, namespace: Vector[String], owner: Option[Symbol], tpe: Type) extends TypeSymbol(name, namespace, owner, tpe)
-  object StructSymbol {
-    def apply(name: String, namespace: Vector[String], tpe: Type, owner: Option[Symbol] = None): StructSymbol =
-      new StructSymbol(name, namespace, owner, tpe)
-  }
-  */
-
-  class TermSymbol(
-    val name: String,
-    val namespace: Vector[String],
-    owner: Option[Symbol],
-    tpe: Type
-  ) extends Symbol(owner, tpe)
   object TermSymbol {
-    def apply(name: String, namespace: Vector[String], tpe: Type, owner: Option[Symbol]): TermSymbol =
-      new TermSymbol(name, namespace, owner, tpe)
+    def apply(name: String, tpe: Type, ctx: Context): TermSymbol =
+      new TermSymbol(name, tpe, ctx)
   }
 }
 
