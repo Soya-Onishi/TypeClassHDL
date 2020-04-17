1ddcb6828896a328f76467a427fc29dcd49b4e33
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-04-17 17:40:04 +0900

    Fix Scope more simple
    For now, type symbol and term symbol will be stored in same table
    This may cause name conflict between type and term, but it should be rare under consistent naming rules

--- src/main/scala/tchdl/util/Scope.scala
+++ src/main/scala/tchdl/util/Scope.scala
@@ -1,54 +1,23 @@
 package tchdl.util
 
-import scala.annotation.tailrec
 import scala.collection.mutable
 
-class Scope(parent: Option[Scope]) {
-  private val tpetab = mutable.HashMap[String, Type]()
-  private val symtab = mutable.HashMap[String, Symbol]()
+class Scope {
+  private val table = mutable.HashMap[String, Symbol]()
 
-  def appendSym(symbol: Symbol): Either[Error, Unit] = {
-    append(symbol.name, symbol, symtab)
-  }
-
-  def lookupSym(name: String): Either[Error, Symbol] = {
-    lookup(name, symtab)
-  }
-
-  def appendType(tpe: Type): Either[Error, Unit] = {
-    append(tpe.name, tpe, tpetab)
-  }
-
-  def lookupType(name: String): Either[Error, Type] = {
-    lookup(name, tpetab)
-  }
-
-  private def append[T](name: String, sym: T, table: mutable.HashMap[String, T]): Either[Error, Unit] = {
-    if(table.contains(name)) Left(???)
+  def append(symbol: Symbol): Either[Error, Unit] = {
+    if (table.contains(symbol.name)) Left(???)
     else {
-      table(name) = sym
+      table(symbol.name) = symbol
       Right(())
     }
   }
 
-  @tailrec
-  private def lookup[T](name: String, table: mutable.HashMap[String, T]): Either[Error, T] = {
-    table.get(name) match {
-      case Some(elem) => Right(elem)
-      case None => parent match {
-        case Some(p) => p.lookup(name, table)
-        case None => Left(???)
-      }
-    }
+  def lookup(name: String): Option[Symbol] = {
+    table.get(name)
   }
 }
 
 object Scope {
-  def apply(parent: Scope): Scope = {
-    new Scope(Some(parent))
-  }
-
-  def root(): Scope = {
-    new Scope(None)
-  }
+  def empty = new Scope
 }
