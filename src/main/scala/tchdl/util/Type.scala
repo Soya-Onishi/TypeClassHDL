2adf859ddf557422c516c698ebe362564651921d
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-04-17 17:42:20 +0900

    Add type generation method
    This method is WIP

--- src/main/scala/tchdl/util/Type.scala
+++ src/main/scala/tchdl/util/Type.scala
@@ -1,94 +1,158 @@
 package tchdl.util
 
-import tchdl.ast.{Expression, Definition}
-import tchdl.util.Context
-import scala.collection.immutable
+import tchdl.ast._
+import tchdl.typecheck.Namer
 
 trait Type {
   def name: String
-  def namespace: Vector[String]
-  def declares: Map[String, Symbol]
-  def tpeType: TpeType
-  def tpeClass: TpeClass
+  def namespace: NameSpace
+  def declares: Scope
 
-  def returnType: Type
+  def asRefType: Type.RefType = this.asInstanceOf[Type.RefType]
 }
 
 object Type {
-  case class TypeGenerator(ctx: Context, tree: Definition) extends Type {
+  class TypeGenerator(tree: Definition, ctx: Context) extends Type {
     val name = "<?>"
-    val namespace = ctx.namespace
-    def declares = throw new IllegalAccessException("TypeGenerator prohibits an access of 'declares'")
-    def tpeType = throw new IllegalAccessException("TypeGenerator prohibits an access of 'tpeType'")
-    def tpeClass = throw new IllegalAccessException("TypeGenerator prohibits an access of 'tpeClass'")
+    def declares = throw new TchdlException.ImplimentationErrorException("TypeGenerator prohibits an access of 'declares'")
+    def namespace = throw new TchdlException.ImplimentationErrorException("TypeGenerator prohibits an access of 'namespace'")
 
-    def returnType = throw new IllegalAccessException("TypeGenerator prohibits an access of 'returnType'")
+    def generate(): Type = tree match {
+      case module: ModuleDef =>
+        val paramCtx = Context(ctx, module.name)
+        val namedHp = module.hp.map(Namer.named(_, paramCtx)).map(_.symbol.asTermSymbol)
+        val namedTp = module.tp.map(Namer.named(_, paramCtx)).map(_.symbol.asTypeSymbol)
 
-    def generate(): Type = ???
+        // TODO: Need Typer for hp and tp
+
+        val componentCtx = Context(paramCtx, module.name)
+        module.components.map(Namer.named(_, componentCtx))
+
+        // TODO: Need Typer for components
+
+        DeclaredType(module.name, ctx.namespace, namedHp, namedTp, componentCtx.scope)
+      case struct: StructDef =>
+        val paramCtx = Context(ctx, struct.name)
+        val namedHp = struct.hp.map(Namer.named(_, paramCtx)).map(_.symbol.asTermSymbol)
+        val namedTp = struct.tp.map(Namer.named(_, paramCtx)).map(_.symbol.asTypeSymbol)
+
+        // TODO: Need Typer for hp and tp
+
+        val fieldCtx = Context(paramCtx, struct.name)
+        struct.fields.map(Namer.named(_, fieldCtx))
+
+        // TODO: Need Typer for fields
+
+        DeclaredType(struct.name, ctx.namespace, namedHp, namedTp, fieldCtx.scope)
+      case method: MethodDef =>
+        val paramCtx = Context(ctx, method.name)
+        val namedHp = method.hp.map(Namer.named(_, paramCtx)).map(_.symbol.asTermSymbol)
+        val namedTp = method.tp.map(Namer.named(_, paramCtx)).map(_.symbol.asTypeSymbol)
+        val namedParam = method.params
+          .map(Namer.named(_, paramCtx))
+          .map(_.symbol.asTermSymbol)
+
+        /* TODO: Need Typer for hp, tp, parameter and ret type.
+         *       If there is no ret type, need to analyze method's expression.
+         *       Or, write return type in force for user
+         *       (i.e. if there is no ret type explicitly, it is error)
+         */
+
+
+        ???
+      case vdef: ValDef =>
+        val ValDef(_, _, tpeTree, expr) = vdef
+
+        (tpeTree, expr) match {
+          case (None, None) => ??? // TODO: append error that represents there is no type and expression
+          case (None, Some(expr)) => ??? // TODO: Need Typer for expr
+          case (Some(tpe), _) => ??? // TODO: Need Typer for tpe
+        }
+      case stage: StageDef =>
+        val paramCtx = Context(ctx, stage.name)
+        val namedParams = stage.params.map(Namer.named(_, paramCtx)).map(_.symbol.asTermSymbol)
+
+        // TODO: Need Typer for parameters and return types
+
+        ???
+    }
   }
 
-  case class DeclaredType(
-    name: String,
-    namespace: Vector[String],
-    hardwareParam: Vector[Symbol],
-    typeParam: Vector[Symbol],
-    declares: Map[String, Symbol],
-    tpeType: TpeType,
-    tpeClass: TpeClass
+  object TypeGenerator {
+    def apply(tree: Definition, ctx: Context): TypeGenerator = new TypeGenerator(tree, ctx)
+  }
+
+  class DeclaredType(
+    val name: String,
+    val namespace: NameSpace,
+    val hardwareParam: Vector[Symbol.TermSymbol],
+    val typeParam: Vector[Symbol.TypeSymbol],
+    val declares: Scope,
   ) extends Type {
     def returnType: Type = this
   }
 
-  case class MethodType(
-    args: Vector[RefType],
-    returnType: RefType,
-    hardwareParam: Vector[Symbol],
-    typeParam: Vector[Symbol],
+  object DeclaredType {
+    def apply(
+               name: String,
+               namespace: NameSpace,
+               hp: Vector[Symbol.TermSymbol],
+               tp: Vector[Symbol.TypeSymbol],
+               declares: Scope
+             ): DeclaredType =
+      new DeclaredType(name, namespace, hp, tp, declares)
+
+  }
+
+  class MethodType(
+    val args: Vector[RefType],
+    val returnType: RefType,
+    val hardwareParam: Vector[Symbol.TermSymbol],
+    val typeParam: Vector[Symbol.TypeSymbol],
   ) extends Type {
     lazy val name: String = {
       val argTypeNames = args.map(_.name).mkString(", ")
       s"($argTypeNames) -> ${returnType.name}"
     }
 
-    val namespace: Vector[String] = Vector()
-    val declares = returnType.declares
-    val tpeType = TpeType.Method
-    val tpeClass = TpeClass.Software
+    val namespace: NameSpace = NameSpace.empty
+    val declares: Scope = returnType.declares
   }
 
-  case class RefType(
-    origin: DeclaredType,
-    hardwareParam: Vector[Expression],
-    typeParam: Vector[RefType]
+  object MethodType {
+    def apply(
+               args: Vector[RefType],
+               retTpe: RefType,
+               hp: Vector[Symbol.TermSymbol],
+               tp: Vector[Symbol.TypeSymbol]
+             ): MethodType =
+      new MethodType(args, retTpe, hp, tp)
+  }
+
+  class RefType(
+    val origin: Symbol.TypeSymbol,
+    val hardwareParam: Vector[Expression],
+    val typeParam: Vector[RefType]
   ) extends Type {
     val name: String = origin.name
-    val namespace: Vector[String] = origin.namespace
-    val declares: Map[String, Symbol] = origin.declares
-    val tpeType: TpeType = origin.tpeType
-    def tpeClass: TpeClass = ???
-
-    def returnType: Type = this
+    val namespace: NameSpace = origin.namespace
+    val declares: Scope = origin.tpe.declares
   }
-}
 
-sealed trait TpeType
-object TpeType {
-  case object Module extends TpeType
-  case object Struct extends TpeType
-  case object Enum extends TpeType
-  case object Class extends TpeType
-  case object Interface extends TpeType
-  case object Implement extends TpeType
-  case object Method extends TpeType
-  case object TypeParam extends TpeType
-}
+  object RefType {
+    def apply(origin: Symbol.TypeSymbol, hp: Vector[Expression], tp: Vector[RefType]): RefType =
+      new RefType(origin, hp, tp)
+  }
 
-sealed trait TpeClass
-object TpeClass {
-  case object Software extends TpeClass
-  case object Hardware extends TpeClass
-  case object Module extends TpeClass
-  case object Unknown extends TpeClass
-  case class Mixed(set: immutable.Set[TpeClass]) extends TpeClass
-  case class DependsOn(symbols: Vector[Symbol]) extends TpeClass
+  object NoType extends Type {
+    val name: String = "no type"
+    val namespace: NameSpace = NameSpace.empty
+    val declares: Scope = Scope.empty
+  }
+
+  object ErrorType extends Type {
+    val name: String = "error type"
+    val namespace: NameSpace = NameSpace.empty
+    val declares: Scope = Scope.empty
+  }
 }
